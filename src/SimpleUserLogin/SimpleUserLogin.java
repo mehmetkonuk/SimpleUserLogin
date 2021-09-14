8d7862f7da94adc4538767d0d33108e825309db5
mehmetkonuk <mehmetkonuk42@yandex.com>
2021-09-14 20:15:29 +0300

    Initial commit

---
+++ src/SimpleUserLogin/SimpleUserLogin.java
@@ -0,0 +1,11 @@
+package SimpleUserLogin;
+
+public class SimpleUserLogin
+{
+	public static void main(String[] args)
+	{
+		String adi;
+		adi="mehmet";
+		System.out.println("Kullanýcý adý:"+adi);
+	}
+}
